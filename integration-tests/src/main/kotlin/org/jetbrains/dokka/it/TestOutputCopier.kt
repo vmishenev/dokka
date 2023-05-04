0f8229d5a446eb7f1ad96a651e5ad58f964405c6
vmishenev <vad-mishenev@yandex.ru>
2023-05-05 01:22:18 +0300

    Move instead of copy

--- integration-tests/src/main/kotlin/org/jetbrains/dokka/it/TestOutputCopier.kt
+++ integration-tests/src/main/kotlin/org/jetbrains/dokka/it/TestOutputCopier.kt
@@ -10,7 +10,8 @@ interface TestOutputCopier {
     fun copyToLocation() {
         System.getenv("DOKKA_TEST_OUTPUT_PATH")?.also { location ->
             println("Copying to ${File(location).absolutePath}")
-            projectOutputLocation.copyRecursively(File(location))
+            projectOutputLocation.renameTo(File(location))
+            //projectOutputLocation.copyRecursively(File(location))
         } ?: println("No path via env. varbiable 'DOKKA_TEST_OUTPUT_PATH' provided, skipping copying")
     }
 }

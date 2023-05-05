e515ed987c89600e38a87d0929b2a7fcf2bc0ba3
vmishenev <vad-mishenev@yandex.ru>
2023-05-05 03:37:55 +0300

    Revert `TestOutputCopier`

--- integration-tests/src/main/kotlin/org/jetbrains/dokka/it/TestOutputCopier.kt
+++ integration-tests/src/main/kotlin/org/jetbrains/dokka/it/TestOutputCopier.kt
@@ -10,8 +10,7 @@ interface TestOutputCopier {
     fun copyToLocation() {
         System.getenv("DOKKA_TEST_OUTPUT_PATH")?.also { location ->
             println("Copying to ${File(location).absolutePath}")
-            projectOutputLocation.renameTo(File(location))
-            //projectOutputLocation.copyRecursively(File(location))
+            projectOutputLocation.copyRecursively(File(location))
         } ?: println("No path via env. varbiable 'DOKKA_TEST_OUTPUT_PATH' provided, skipping copying")
     }
 }
